f9cddee711973bee19416a0e2a2571dff17a1260
Jinghan <phantom.there@gmail.com>
2016-10-01 03:59:35 +0800

    color manager

---
+++ FitMi/FitMi/Services/FMColorManager.swift
@@ -0,0 +1,16 @@
+//
+//  FMColorManager.swift
+//  FitMi
+//
+//  Created by Jinghan Wang on 1/10/16.
+//  Copyright © 2016 FitMi. All rights reserved.
+//
+
+import UIKit
+
+class FMColorManager: NSObject {
+	static let primaryColor = UIColor.black
+	static let secondaryColor = UIColor.white
+	static let darkTextColor = UIColor.black
+	static let lightTextColor = UIColor.white
+}
