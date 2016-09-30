0a6341f897490de78b1d3f57991b6361f9e9bf14
Jinghan <phantom.there@gmail.com>
2016-10-01 03:23:21 +0800

    pod install

---
+++ FitMi/Pods/Target Support Files/Pods-FitMi/Pods-FitMi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FitMi : NSObject
+@end
+@implementation PodsDummy_Pods_FitMi
+@end
---
+++ FitMi/Pods/Target Support Files/Pods-FitMiUITests/Pods-FitMiUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FitMiUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FitMiUITests
+@end
