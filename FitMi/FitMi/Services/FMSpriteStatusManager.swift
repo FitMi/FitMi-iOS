d0a7751377719208e56bf15bc1a4e50ec37baafd
Jinghan <phantom.there@gmail.com>
2016-10-01 22:03:09 +0800

    add sprite status manager

---
+++ FitMi/FitMi/Services/FMSpriteStatusManager.swift
@@ -0,0 +1,13 @@
+//
+//  FMSpriteStatusManager.swift
+//  FitMi
+//
+//  Created by Jinghan Wang on 1/10/16.
+//  Copyright © 2016 FitMi. All rights reserved.
+//
+
+import UIKit
+
+class FMSpriteStatusManager: NSObject {
+	static let sharedManager = FMSpriteStatusManager()
+}
