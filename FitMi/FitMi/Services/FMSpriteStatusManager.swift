9499d1c2d9b84528cd0a5442a0da9d312a4034fe
Jinghan <phantom.there@gmail.com>
2016-10-03 19:18:10 +0800

    skeleton for sprite status manager

--- FitMi/FitMi/Services/FMSpriteStatusManager.swift
+++ FitMi/FitMi/Services/FMSpriteStatusManager.swift
@@ -10,4 +10,55 @@ import UIKit
 
 class FMSpriteStatusManager: NSObject {
 	static let sharedManager = FMSpriteStatusManager()
+	
+	func createNewSprite() -> FMSprite {
+		// TODO: Set correct initial values
+		return FMSprite()
+	}
+	
+	func currentStrength() -> Double {
+		return 0
+	}
+	
+	func currentStamina() -> Double {
+		return 0
+	}
+	
+	func currentAgility() -> Double {
+		return 0
+	}
+	
+	func currentExperience() -> Double {
+		return 0
+	}
+	
+	func currentLevel() -> Double {
+		return 0
+	}
+	
+	func currentMood() -> Double {
+		// TODO: check the time of last data point and calculate current mode
+		return 50
+	}
+	
+	func currentHP() -> Double {
+		return 0
+	}
+	
+	func updateSpriteStatus(steps: Double, distance: Double, flights: Double, date: Date) {
+		// TODO: Check whether data exist for the date
+		// If so, replace the old data
+		// If not, add in the new data
+	}
+	
+	// Return value indicates whether the operation is successful
+	func increaseMood() -> Bool {
+		// TODO: check the time of last data point and see whether increase mood is allowed
+		// TODO: check whether the mood is full
+		return false
+	}
+	
+	func increaseExperience(increment: Double) {
+		
+	}
 }
