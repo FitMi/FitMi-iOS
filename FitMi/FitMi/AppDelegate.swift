fe3bfe4a771bb785dd189847e54e201c0077c89f
Jiang Sheng <gisonrg@gmail.com>
2016-10-16 18:16:38 +0800

    Add Facebook login app delegate.

--- FitMi/FitMi/AppDelegate.swift
+++ FitMi/FitMi/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import FontBlaster
+import FacebookCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -30,8 +31,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         self.window?.makeKeyAndVisible()
 		
-		return true
+		return SDKApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
 	}
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        return SDKApplicationDelegate.shared.application(app, open: url, options: options)
+    }
 
 	func applicationWillResignActive(_ application: UIApplication) {
 		// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
