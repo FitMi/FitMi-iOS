8d2ae3d6e2697211c6d0b87b242cdf48560a67ca
Jinghan Wang <jinghan.w@icloud.com>
2016-10-22 20:52:29 +0800

    damage label update

--- FitMi/FitMi/Services/FMNotificationManager.swift
+++ FitMi/FitMi/Services/FMNotificationManager.swift
@@ -40,7 +40,39 @@ class FMNotificationManager: NSObject {
 				_ in
 				messageView.removeFromSuperview()
 		})
+	}
+	
+	func showFeedbackMessage(text: String, in view: UIView, offset: CGPoint, borderColor: UIColor, textColor: UIColor) {
+		let messageView = FMOutlinedLabel(frame: view.bounds)
+		messageView.isUserInteractionEnabled = false
+		messageView.textAlignment = .center
+		messageView.font = UIFont(name: "Pixeled", size: 20)
+		messageView.textColor = textColor
+		messageView.outlineColor = borderColor
+		messageView.outlineWidth = 2
+		messageView.text = text
 		
+		view.addSubview(messageView)
 		
+		var newCenter = messageView.center
+		newCenter.x = newCenter.x + offset.x
+		newCenter.y = newCenter.y + offset.y
+		messageView.center = newCenter
+		
+		newCenter.y += 150
+		
+		UIView.animate(withDuration: 2, delay: 0, options: [.curveEaseIn], animations: {
+			messageView.center = newCenter
+			}, completion: {
+				_ in
+				messageView.removeFromSuperview()
+		})
+		
+		UIView.animate(withDuration: 0.3, delay: 0.2, options: [.curveEaseIn], animations: {
+			messageView.alpha = 0
+			}, completion: {
+				_ in
+				messageView.removeFromSuperview()
+		})
 	}
 }
