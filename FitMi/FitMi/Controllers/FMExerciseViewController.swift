db59e8f2144c98b35d2ae065eba905b58edf7087
Jinghan Wang <jinghan.w@icloud.com>
2016-10-25 22:05:52 +0800

    exercise view ui adjustment

--- FitMi/FitMi/Controllers/FMExerciseViewController.swift
+++ FitMi/FitMi/Controllers/FMExerciseViewController.swift
@@ -172,7 +172,6 @@ class FMExerciseViewController: FMViewController {
 	
 	func highlightPanel() {
 		self.highlightBackground.isHidden = false
-		let loggedIn = UserDefaults.standard.string(forKey: "jwt") != nil
 		UIView.animate(withDuration: 0.1, delay: 0, options: [], animations: {
 			self.spriteView.alpha = 0
 			self.highlightBackground.alpha = 1
@@ -180,17 +179,22 @@ class FMExerciseViewController: FMViewController {
 			_ in
 			self.view.setNeedsLayout()
 			UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0, options: [.curveEaseInOut], animations: {
-				self.statusPanelViewTopConstraint.constant = UIScreen.main.bounds.height / 2 - 200
-				if loggedIn {
-					self.stackViewButtonHeightConstraint.constant = 50
-					self.stackViewButtonBottomConstraint.constant = 15
-				}
+				self.statusPanelViewTopConstraint.constant = UIScreen.main.bounds.height / 2 - 230
+				self.stackViewButtonHeightConstraint.constant = 50
+				self.stackViewButtonBottomConstraint.constant = 15
+				self.labelExperience.superview?.isHidden = false
 				self.view.layoutIfNeeded()
             }, completion: nil)
 		})
 	}
     
     @IBAction func facebookShare() {
+		let loggedIn = UserDefaults.standard.string(forKey: "jwt") != nil
+		if !loggedIn {
+			FMNotificationManager.sharedManager.showStandardFeedbackMessage(text: "Go to Settings page to log in.")
+			return
+		}
+		
         let duration = Int(self.exerciseEndDate.timeIntervalSince(self.exerciseStartDate))
         let graphProperties = ["og:type": "fitness.course",
                                "og:title": "WOW, I ran with FitMi today!",
@@ -226,13 +230,11 @@ class FMExerciseViewController: FMViewController {
 		})
 		
 		self.reset(nil)
-		let loggedIn = UserDefaults.standard.string(forKey: "jwt") != nil
 		self.view.setNeedsLayout()
 		UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0, options: [.curveEaseInOut], animations: {
-			if loggedIn {
-				self.stackViewButtonHeightConstraint.constant = 0
-				self.stackViewButtonBottomConstraint.constant = 5
-			}
+			self.stackViewButtonHeightConstraint.constant = 0
+			self.stackViewButtonBottomConstraint.constant = 5
+			self.labelExperience.superview?.isHidden = true
 			self.statusPanelViewTopConstraint.constant = 40
 			self.view.layoutIfNeeded()
         }, completion: nil)
