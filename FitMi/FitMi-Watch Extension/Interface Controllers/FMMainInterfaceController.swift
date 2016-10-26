e68bd4adcf56de147d783742d819ee80e3169c3e
Jinghan Wang <jinghan.w@icloud.com>
2016-10-27 03:54:32 +0800

    link core motion data

--- FitMi/FitMi-Watch Extension/Interface Controllers/FMMainInterfaceController.swift
+++ FitMi/FitMi-Watch Extension/Interface Controllers/FMMainInterfaceController.swift
@@ -8,11 +8,94 @@
 
 import UIKit
 import WatchKit
+import CoreMotion
 
 class FMMainInterfaceController: WKInterfaceController {
 	
+	@IBOutlet var durationTimer: WKInterfaceTimer!
+	@IBOutlet var stepValueLabel: WKInterfaceLabel!
+	@IBOutlet var meterValueLabel: WKInterfaceLabel!
+	@IBOutlet var floorValueLabel: WKInterfaceLabel!
+	
+	@IBOutlet var buttonTitleLabel: WKInterfaceLabel!
+	
+	fileprivate var exerciseStarted = false
+	
+	fileprivate var startDate: Date!
+	fileprivate var endDate: Date!
+	fileprivate var stepCount: Int = 0
+	fileprivate var meterCount: Int = 0
+	fileprivate var floorCount: Int = 0
+	
+	fileprivate var pedometer = CMPedometer()
+	
 	override func awake(withContext context: Any?) {
 		super.awake(withContext: context)
+	}
+	
+	@IBAction func toggleExercise(sender: WKInterfaceButton) {
+		if !exerciseStarted {
+			WKInterfaceDevice.current().play(.start)
+			startDate = Date()
+			durationTimer.setDate(startDate)
+			durationTimer.start()
+			buttonTitleLabel.setText("END EXERCISE")
+			
+			self.startMotionUpdates()
+		} else {
+			endDate = Date()
+			WKInterfaceDevice.current().play(.stop)
+			durationTimer.stop()
+			buttonTitleLabel.setText("START EXERCISE")
+			
+			self.endMotionUpdates()
+			self.generateExerciseReport()
+		}
+		exerciseStarted = !exerciseStarted
+	}
+	
+	func generateExerciseReport() {
+		print(startDate)
+		print(endDate)
+	}
+	
+	func updateView(from pedometerData: CMPedometerData) {
+		stepCount = Int(pedometerData.numberOfSteps)
+		
+		if CMPedometer.isDistanceAvailable() {
+			meterCount = Int(pedometerData.distance!)
+		}
+		
+		if CMPedometer.isFloorCountingAvailable() {
+			floorCount = Int(pedometerData.floorsAscended!)
+		}
+		
+		self.updateView()
+	}
+	
+	func updateView() {
+		self.stepValueLabel.setText("\(stepCount)")
+		self.meterValueLabel.setText("\(meterCount)")
+		self.floorValueLabel.setText("\(floorCount)")
+	}
+}
 
+extension FMMainInterfaceController {
+	func startMotionUpdates() {
+		let currentDate = Date()
+		pedometer.startUpdates(from: currentDate) {
+			(data, error) in
+			if error != nil {
+				print(error!)
+			} else {
+				if let motionData: CMPedometerData = data {
+					self.updateView(from: motionData)
+				}
+			}
+		}
+	}
+	
+	func endMotionUpdates() {
+		pedometer.stopUpdates()
 	}
 }
