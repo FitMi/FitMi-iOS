ca35e762fb58d03c04a6ae9fb11909115ff400e0
Jinghan Wang <jinghan.w@icloud.com>
2017-01-18 21:32:43 +0800

    basic UI testing

--- FitMi/FitMiUITests/FitMiUITests.swift
+++ FitMi/FitMiUITests/FitMiUITests.swift
@@ -31,6 +31,33 @@ class FitMiUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+		
+		let app = XCUIApplication()
+		app.otherElements.containing(.image, identifier:"icon").element.tap()
+		app.buttons["home goal"].tap()
+		app.buttons["EDIT GOAL"].tap()
+		app.buttons["DONE"].tap()
+		
+		let dismissButton = app.buttons["DISMISS"]
+		dismissButton.tap()
+		
+		let element = app.otherElements.containing(.button, identifier:"=").children(matching: .other).element.children(matching: .other).element(boundBy: 1)
+		element.tap()
+		element.press(forDuration: 0.9);
+		element.tap()
+		element.tap()
+		element.tap()
+		element.tap()
+		element.tap()
+		element.tap()
+		element.tap()
+		element.press(forDuration: 0.4);
+		app.buttons["home booth"].tap()
+		
+		let tablesQuery = app.tables
+		tablesQuery.staticTexts["Polar Bear"].tap()
+		tablesQuery.staticTexts["Brown Bear"].tap()
+		dismissButton.tap()
     }
     
 }
